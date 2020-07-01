5ed4a55ecf018e83c91e09b6eb12d5fe434f6dff
Tulio Faria <tuliofaria@gmail.com>
2020-07-01 18:58:19 -0300

    adding cors.

--- pages/api/getUser.js
+++ pages/api/getUser.js
@@ -1,6 +1,18 @@
 import getUser from '../../utils/getUser'
+import Cors from 'cors'
+import initMiddleware from '../../lib/init-middleware'
 
-const apiGetUser = async(req, res) => {
+const cors = initMiddleware(
+    // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+    Cors({
+        // Only allow requests with GET, POST and OPTIONS
+        methods: ['GET', 'OPTIONS'],
+    })
+)
+
+const apiGetUser = async (req, res) => {
+    await cors(req, res)
+
     const data = await getUser('tuliofaria')
     res.send(data)
 }
